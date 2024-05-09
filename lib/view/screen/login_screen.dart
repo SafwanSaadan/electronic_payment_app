74bf3c2f9c2cb999ca349641053837a08c4ca088
SafwanSaadan <safwansadaan7@gmail.com>
2024-05-10 02:09:12 +0300

    update

--- lib/view/screen/login_screen.dart
+++ lib/view/screen/login_screen.dart
@@ -22,101 +22,67 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<FloatingController>(
-      builder: (controller) => Scaffold(
-        floatingActionButton: FloatingActionButtonLogin(),
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(bgList[controller.selectedIndex]),
-                fit: BoxFit.fill),
-          ),
-          alignment: Alignment.center,
-          child: Container(
-            height: 450,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: GetBuilder<FloatingController>(
+        builder: (controller) => Scaffold(
+          floatingActionButton: FloatingActionButtonLogin(),
+          body: Container(
+            height: double.infinity,
             width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 30),
             decoration: BoxDecoration(
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.black.withOpacity(0.1),
+              image: DecorationImage(
+                  image: AssetImage(bgList[controller.selectedIndex]),
+                  fit: BoxFit.fill),
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaY: 5, sigmaX: 5),
-                  child: Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Spacer(),
-                        Center(
-                            child: TextUtil(
-                          text: "Login",
-                          weight: true,
-                          size: 30,
-                        )),
-                        const Spacer(),
-                        TextUtil(
-                          text: "اسم الدخول",
-                        ),
-                        Container(
-                          height: 35,
-                          decoration: const BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.white))),
-                          child: TextFormField(
-                            controller: controller.emailController,
-                            keyboardType: TextInputType.emailAddress,
-                            style: const TextStyle(color: Colors.white),
-                            decoration: const InputDecoration(
-                              // label: Text("Email"),
-                              hintText: "اسم الدخول",
-                              prefixIcon: Icon(
-                                Icons.person_pin,
-                                color: Colors.white,
-                              ),
-                              suffixIcon: Icon(
-                                Icons.mail,
-                                color: Colors.white,
-                              ),
-                              fillColor: Colors.white,
-                              border: InputBorder.none,
-                            ),
+            alignment: Alignment.center,
+            child: Container(
+              height: 450,
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.black.withOpacity(0.1),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaY: 5, sigmaX: 5),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Spacer(),
+                          Center(
+                              child: TextUtil(
+                            text: "Login",
+                            weight: true,
+                            size: 30,
+                          )),
+                          const Spacer(),
+                          TextUtil(
+                            text: "اسم الدخول",
                           ),
-                        ),
-                        const Spacer(),
-                        TextUtil(
-                          text: "كلمة المرور",
-                        ),
-                        Container(
-                          height: 35,
-                          decoration: const BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(color: Colors.white))),
-                          child: GetBuilder<FloatingController>(
-                            builder: (controller) => TextFormField(
-                              controller: controller.passwordController,
-                              obscureText: controller.isPassword,
-                              keyboardType: TextInputType.visiblePassword,
+                          Container(
+                            height: 35,
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.white))),
+                            child: TextFormField(
+                              controller: controller.emailController,
+                              keyboardType: TextInputType.emailAddress,
                               style: const TextStyle(color: Colors.white),
-                              decoration: InputDecoration(
-                                hintText: "كلمة المرور",
-                                suffixIcon: IconButton(
-                                  icon: controller.isPassword == true
-                                      ? const Icon(Icons.visibility_off,
-                                          color: Colors.white)
-                                      : const Icon(Icons.remove_red_eye,
-                                          color: Colors.white),
-                                  onPressed: () {
-                                    controller.visibilityOff();
-                                  },
+                              decoration: const InputDecoration(
+                                // label: Text("Email"),
+                                hintText: "اسم الدخول",
+                                prefixIcon: Icon(
+                                  Icons.person_pin,
+                                  color: Colors.white,
                                 ),
-                                prefixIcon: const Icon(
-                                  Icons.lock,
+                                suffixIcon: Icon(
+                                  Icons.mail,
                                   color: Colors.white,
                                 ),
                                 fillColor: Colors.white,
@@ -124,90 +90,127 @@ class LoginScreen extends StatelessWidget {
                               ),
                             ),
                           ),
-                        ),
-                        const Spacer(),
-                        InkWell(
-                          onTap: () {
-                            controller.checkedbox();
-                          },
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 15,
-                                width: 15,
-                                color: Colors.white,
-                                child: GetBuilder<FloatingController>(
-                                  builder: (controller) => Checkbox(
-                                      value: controller.chack,
-                                      onChanged: (val) {
-                                        controller.checkedbox();
-                                      }),
+                          const Spacer(),
+                          TextUtil(
+                            text: "كلمة المرور",
+                          ),
+                          Container(
+                            height: 35,
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.white))),
+                            child: GetBuilder<FloatingController>(
+                              builder: (controller) => TextFormField(
+                                controller: controller.passwordController,
+                                obscureText: controller.isPassword,
+                                keyboardType: TextInputType.visiblePassword,
+                                style: const TextStyle(color: Colors.white),
+                                decoration: InputDecoration(
+                                  hintText: "كلمة المرور",
+                                  suffixIcon: IconButton(
+                                    icon: controller.isPassword == true
+                                        ? const Icon(Icons.visibility_off,
+                                            color: Colors.white)
+                                        : const Icon(Icons.remove_red_eye,
+                                            color: Colors.white),
+                                    onPressed: () {
+                                      controller.visibilityOff();
+                                    },
+                                  ),
+                                  prefixIcon: const Icon(
+                                    Icons.lock,
+                                    color: Colors.white,
+                                  ),
+                                  fillColor: Colors.white,
+                                  border: InputBorder.none,
                                 ),
                               ),
-                              const SizedBox(
-                                width: 10,
+                            ),
+                          ),
+                          const Spacer(),
+                          InkWell(
+                            onTap: () {
+                              controller.checkedbox();
+                            },
+                            child: Row(
+                              children: [
+                                Container(
+                                  height: 15,
+                                  width: 15,
+                                  color: Colors.white,
+                                  child: GetBuilder<FloatingController>(
+                                    builder: (controller) => Checkbox(
+                                        value: controller.chack,
+                                        onChanged: (val) {
+                                          controller.checkedbox();
+                                        }),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                    child: TextUtil(
+                                  text: "تذكر كلمة المرور",
+                                  size: 12,
+                                  weight: true,
+                                ))
+                              ],
+                            ),
+                          ),
+                          const Spacer(),
+                          InkWell(
+                            onTap: () {
+                              sharedPref!.setString(
+                                  'username', controller.emailController.text);
+                              sharedPref!.setString('password',
+                                  controller.passwordController.text);
+                              print("===============");
+                              Get.toNamed(AppRoutes.onBoarding);
+                            },
+                            child: Container(
+                              height: 40,
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30)),
+                              alignment: Alignment.center,
+                              child: TextUtil(
+                                text: "دخول",
+                                color: Colors.black,
                               ),
-                              Expanded(
-                                  child: TextUtil(
-                                text: "تذكر كلمة المرور",
+                            ),
+                          ),
+                          const Spacer(),
+                          Center(
+                              child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextUtil(
+                                text: "إذا لم تكن تمتلك حساب إضغط هنا ",
                                 size: 12,
                                 weight: true,
-                              ))
+                              ),
+                              const SizedBox(
+                                width: 5,
+                              ),
+                              InkWell(
+                                  onTap: () {
+                                    print("======================");
+                                  },
+                                  child: const Text(
+                                    'إنشاء حساب',
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  )),
                             ],
-                          ),
-                        ),
-                        const Spacer(),
-                        InkWell(
-                          onTap: () {
-                            sharedPref!.setString(
-                                'username', controller.emailController.text);
-                            sharedPref!.setString(
-                                'password', controller.passwordController.text);
-                            print("===============");
-                            Get.toNamed(AppRoutes.onBoarding);
-                          },
-                          child: Container(
-                            height: 40,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(30)),
-                            alignment: Alignment.center,
-                            child: TextUtil(
-                              text: "دخول",
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                        const Spacer(),
-                        Center(
-                            child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            TextUtil(
-                              text: "إذا لم تكن تمتلك حساب إضغط هنا ",
-                              size: 12,
-                              weight: true,
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            InkWell(
-                                onTap: () {
-                                  print("======================");
-                                },
-                                child: const Text(
-                                  'إنشاء حساب',
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                  ),
-                                )),
-                          ],
-                        )),
-                        const Spacer(),
-                      ],
-                    ),
-                  )),
+                          )),
+                          const Spacer(),
+                        ],
+                      ),
+                    )),
+              ),
             ),
           ),
         ),
